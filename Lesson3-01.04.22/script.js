84e37210a803477a6e1a4bcc8a582a37614eea03
niddahales <69247370+niddahales@users.noreply.github.com>
2022-04-01 15:02:08 +0300

    Add files via upload

---
+++ Lesson3-01.04.22/script.js
@@ -0,0 +1,23 @@
+console.log("message");
+console.log("I am learning a javascript");
+document.write("message");
+
+alert("Welcome back! ");
+
+let a = 5+4+"6"*5/5;
+// a= "6"+6;
+// a= 6+"6";  It is not about left most or right most derivation.
+// it is about expression.
+console.log(a);
+
+b= "This is my sentence";
+c= b.split(" "); // Cümleleri boşluklara göre ayırarak kelimeleri index yapar.
+console.log(c[0]);
+console.log(b.toUpperCase());
+// null ve undefined farkı --> null memory allocation yapılmıştır, undefined'da yapılmamıştır.
+
+
+function merhaba(){
+  console.log("Hello selo");
+}
+merhaba();
